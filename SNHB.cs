9bda0a7e617325a98489e948b6a37669266a05e8
AylanJ123 <aylanjosue@gmail.com>
2022-09-03 23:51:31 -0600

    First commit

---
+++ SNHB.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using BepInEx.IL2CPP;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace SNHB
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInDependency("com.le4fless.corelib")]
+    [BepInProcess("CoreKeeper.exe")]
+    public class SNHBPlugin : BasePlugin
+    {
+        public static ManualLogSource logger;
+        public override void Load()
+        {
+
+            logger = Log;
+            logger.LogInfo($"{PluginInfo.PLUGIN_NAME} mod is loaded!");
+            Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+            harmony.PatchAll();
+        }
+    }
+}
