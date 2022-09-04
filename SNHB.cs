b403b5c3eb4adf06b0f682f5d5251b36990d8e15
AylanJ123 <aylanjosue@gmail.com>
2022-09-04 01:04:51 -0600

    Stabilization

--- SNHB.cs
+++ SNHB.cs
@@ -10,13 +10,12 @@ namespace SNHB
     [BepInProcess("CoreKeeper.exe")]
     public class SNHBPlugin : BasePlugin
     {
-        public static ManualLogSource logger;
+        static ManualLogSource logger;
         public override void Load()
         {
-
             logger = Log;
             logger.LogInfo($"{PluginInfo.PLUGIN_NAME} mod is loaded!");
-            Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+            Harmony harmony = new(PluginInfo.PLUGIN_GUID);
             harmony.PatchAll();
         }
     }
